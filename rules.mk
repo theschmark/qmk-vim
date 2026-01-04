909f503f21fea993395adfb65c5fa32398384b49
Mark Schadler <mark.schadler@gmail.com>
2026-01-04 19:47:08 +0100

    rules.mk update to support inclusion from userspace

--- rules.mk
+++ rules.mk
@@ -1,8 +1,8 @@
 # note that the order is important here
-SRC += qmk-vim/src/mac_mode.c
-SRC += qmk-vim/src/process_func.c
-SRC += qmk-vim/src/numbered_actions.c
-SRC += qmk-vim/src/motions.c
-SRC += qmk-vim/src/actions.c
-SRC += qmk-vim/src/modes.c
-SRC += qmk-vim/src/vim.c
+SRC += $(USER_PATH)/qmk-vim/src/mac_mode.c
+SRC += $(USER_PATH)/qmk-vim/src/process_func.c
+SRC += $(USER_PATH)/qmk-vim/src/numbered_actions.c
+SRC += $(USER_PATH)/qmk-vim/src/motions.c
+SRC += $(USER_PATH)/qmk-vim/src/actions.c
+SRC += $(USER_PATH)/qmk-vim/src/modes.c
+SRC += $(USER_PATH)/qmk-vim/src/vim.c
